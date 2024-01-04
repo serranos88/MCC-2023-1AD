e61bc9db950c5cd942ae4da330a87ac7ec421b9c
serranos88 <antonio.gs@culiacan.tecnm.mx>
2024-01-03 23:54:51 -0700

    0.0.3

--- mlp/Main.java
+++ mlp/Main.java
@@ -6,4 +6,6 @@ public class Main {
     }
 }
 
-
+//git config --global user.name "serranos88"
+//git config --global user.email "antonio.gs@culiacan.tecnm.mx"
+//git remote set-url origin https://github.com/serranos88/MCC-2023-1AD
